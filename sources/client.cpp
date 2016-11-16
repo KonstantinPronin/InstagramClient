933b427b4966cfa3a9ef7b23ea1d578fbec9c7ce
KonstantinPronin <prosayfer@mail.ru>
2016-11-16 18:41:37 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -9,60 +9,60 @@ namespace Instagram{
 
 	auto Client::check_connection() -> bool {
 		
-        std::string json_data;
-	CURL* EasyCurl = curl_easy_init();
+        	std::string json_data;
+		CURL* EasyCurl = curl_easy_init();
 
-	curl_easy_setopt(EasyCurl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/?access_token=");
-	curl_easy_setopt(EasyCurl, CURLOPT_POSTFIELDS, _settings["access_token"]);
-//	curl_easy_setopt(EasyCurl, CURLOPT_POSTFIELDS, "&callback=CallbackFunction");
-        curl_easy_setopt(EasyCurl, CURLOPT_WRITEFUNCTION, Client::CallbackFunction);
-        curl_easy_setopt(EasyCurl, CURLOPT_WRITEDATA, &json_data);
-        //std::cout << json_data <<std::endl ;
+		curl_easy_setopt(EasyCurl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/?access_token=");
+		curl_easy_setopt(EasyCurl, CURLOPT_POSTFIELDS, _settings["access_token"]);
+		//curl_easy_setopt(EasyCurl, CURLOPT_POSTFIELDS, "&callback=CallbackFunction");
+        	curl_easy_setopt(EasyCurl, CURLOPT_WRITEFUNCTION, Client::CallbackFunction);
+        	curl_easy_setopt(EasyCurl, CURLOPT_WRITEDATA, &json_data);
+        	//std::cout << json_data <<std::endl ;
 	
-	if  (curl_easy_perform(EasyCurl) == CURLE_OK && json_data != "") {
+		if  (curl_easy_perform(EasyCurl) == CURLE_OK && json_data != "") {
        
-        	json user_inf = json::parse(json_data);
-           	curl_easy_cleanup(EasyCurl);
+        		json user_inf = json::parse(json_data);
+           		curl_easy_cleanup(EasyCurl);
         
-           	if (user_inf["meta"]["code"] == "200") 
-                	return true;
-           	else return false; 
-	}
+           		if (user_inf["meta"]["code"] == "200") 
+                		return true;
+           		else return false; 
+		}
     
 
-    	curl_easy_cleanup(EasyCurl);
-        return false;
+    		curl_easy_cleanup(EasyCurl);
+        	return false;
 	} 
 
 	
 	
 	auto Client::get_most_popular_photo() -> std::string {
-        std::string json_data;
-        CURL* EasyCurl = curl_easy_init();
+        	std::string json_data;
+        	CURL* EasyCurl = curl_easy_init();
 
-	curl_easy_setopt(EasyCurl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/follows?access_token=");
-	curl_easy_setopt(EasyCurl, CURLOPT_POSTFIELDS, _settings["access_token"]);
-        curl_easy_setopt(EasyCurl, CURLOPT_WRITEFUNCTION, Client::CallbackFunction);
-        curl_easy_setopt(EasyCurl, CURLOPT_WRITEDATA, &json_data);
+		curl_easy_setopt(EasyCurl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/follows?access_token=");
+		curl_easy_setopt(EasyCurl, CURLOPT_POSTFIELDS, _settings["access_token"]);
+        	curl_easy_setopt(EasyCurl, CURLOPT_WRITEFUNCTION, Client::CallbackFunction);
+        	curl_easy_setopt(EasyCurl, CURLOPT_WRITEDATA, &json_data);
 		
-        if (curl_easy_perform(EasyCurl) == CURLE_OK && json_data != ""){
-            json link_to_photo = (json::parse(json_data))["data"]["link"];
+        	if (curl_easy_perform(EasyCurl) == CURLE_OK && json_data != ""){
+            		json link_to_photo = (json::parse(json_data))["data"]["link"];
             
-            curl_easy_cleanup(EasyCurl);
+            		curl_easy_cleanup(EasyCurl);
 
-            return link_to_photo;
-        }
-        else {
-            curl_easy_cleanup(EasyCurl);
-            return "Error";
-        }
+            		return link_to_photo;
+        	}
+        	else {
+            		curl_easy_cleanup(EasyCurl);
+            		return "Error";
+        	}
 	}
 
-    auto Client::CallbackFunction(char *ptr, size_t size, size_t nmemb, std::string *userdata) -> size_t{
-        size_t realsize = size * nmemb;
-        std::cout << realsize << std::endl;
-        //((std::string*) userdata) -> append(ptr, 0, realsize + 1 );
-        *userdata += *ptr;
-        return realsize;
-    }
+    	auto Client::CallbackFunction(char *ptr, size_t size, size_t nmemb, std::string *userdata) -> size_t{
+        	size_t realsize = size * nmemb;
+        	std::cout << realsize << std::endl;
+        	//((std::string*) userdata) -> append(ptr, 0, realsize + 1 );
+        	*userdata += *ptr;
+        	return realsize;
+    	}
 }

786dd8962f14e193a258cbca7c537b3d2e4c80f2
KonstantinPronin <prosayfer@mail.ru>
2016-11-22 22:52:49 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,14 +4,14 @@
 int main (){
 	std::map <std::string, std::string> settings;
 	
-    	std::cout << "click on the link to get acces_token " << std::endl
+    	std::cout << "click on the link to get access_token " << std::endl
     		  << "https://api.instagram.com/oauth/authorize/?client_id=6fd93ecd973849eabbdb6cba7925d1d1&redirect_uri=https://localhost&response_type=token"
-	    	  << std::endl << " enter your acces_token:  ";
+	    	  << std::endl << " enter your access_token:  ";
 	
 	std::string token;
 	std::cin >> token;
 	
-	settings.insert(std::pair<std::string, std::string> ("acces_token", token));
+	settings.insert(std::pair<std::string, std::string> ("access_token", token));
     	Instagram::Client Example(settings);
     	std::cout << Example.check_connection();
 	std::cout << Examples.get_most_popular_photo();

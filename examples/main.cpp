e93bcf6d5d99fa7a411df450fe3ad9e4e2910a2b
KonstantinPronin <prosayfer@mail.ru>
2016-11-25 23:18:04 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,19 +1,45 @@
 #include <client.hpp>
-#include <iostream>
 
-int main (){
-	std::map <std::string, std::string> settings;
+int main() {
+	try{
+		std::map <std::string, std::string> settings;
+
+		std::cout << "Click on the link to get access_token " << std::endl
+		<< "https://api.instagram.com/oauth/authorize/?client_id=6fd93ecd973849eabbdb6cba7925d1d1&redirect_uri=https://localhost&response_type=token"
+		<< std::endl << "Enter your access_token: ";
+
+		std::string token;
+		std::cin >> token;
 	
-    	std::cout << "click on the link to get access_token " << std::endl
-    		  << "https://api.instagram.com/oauth/authorize/?client_id=6fd93ecd973849eabbdb6cba7925d1d1&redirect_uri=https://localhost&response_type=token"
-	    	  << std::endl << " enter your access_token:  ";
-	
-	std::string token;
-	std::cin >> token;
-	
-	settings.insert(std::pair<std::string, std::string> ("access_token", token));
-    	Instagram::Client Example(settings);
-    	std::cout << Example.check_connection();
-	//std::cout << Examples.get_most_popular_photo();
+		settings.insert(std::pair<std::string, std::string>("access_token", token));
+		Instagram::Client Example(settings);
+
+
+		std::vector<std::string> v{ "avram", "nikol", "micki", "zlatan", "citrus", "cipher" };
+		for (size_t i = 0; i < 6; i++) {
+			Example.push_followers_cause_my_program_doesnt_work(v[i], i);
+		}
+		
+		
+
+		while (true) {
+			std::cin.ignore();
+			std::cout << "> ";
+			std::string command;
+			std::getline(std::cin, command);
+			std::cout << std::endl;
+
+			if (command == "./get_followers") Example.threads_run(false);
+			else if (command == "./get_followers -v") Example.threads_run(true);
+			else if (command == "./q") break;
+			else std::cout << "Wrong command, try again.\n";
+
+		}
+
+	}
+	catch (std::logic_error& e) {
+		std::cout << std::endl << e.what();
+	}
+
 	return 0;
 }
